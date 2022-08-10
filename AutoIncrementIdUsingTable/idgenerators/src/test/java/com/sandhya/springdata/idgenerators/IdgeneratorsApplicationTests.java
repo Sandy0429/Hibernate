9c6548b52c2f4d03b99f3458c37f8a0263fe996d
Sandy0429 <54345790+Sandy0429@users.noreply.github.com>
2022-08-11 00:43:04 +0530

    Add files via upload

---
+++ AutoIncrementIdUsingTable/idgenerators/src/test/java/com/sandhya/springdata/idgenerators/IdgeneratorsApplicationTests.java
@@ -0,0 +1,25 @@
+package com.sandhya.springdata.idgenerators;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.sandhya.springdata.idgenerators.entities.Employee;
+import com.sandhya.springdata.idgenerators.repos.EmployeeRepository;
+
+@SpringBootTest
+class IdgeneratorsApplicationTests {
+
+	@Autowired
+	EmployeeRepository er;
+	@Test
+	void testCreateEmployee() {
+		
+		Employee emp =new Employee();		
+		emp.setName("Deago");
+		
+		er.save(emp);
+		
+	}
+
+}
