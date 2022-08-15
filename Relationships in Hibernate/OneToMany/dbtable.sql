0534679f5e9541b647a266fa93ba70fcbc9a36ef
Sandy0429 <54345790+Sandy0429@users.noreply.github.com>
2022-08-15 20:16:40 +0530

    Create dbtable.sql

---
+++ Relationships in Hibernate/OneToMany/dbtable.sql
@@ -0,0 +1,17 @@
+use mydb;
+
+create table customer(
+id int primary key auto_increment,
+name varchar(20)
+);
+
+create table phone_number(
+id int primary key auto_increment,
+customer_id int,
+number varchar(20),
+type varchar(20),
+foreign key(customer_id) references customer(id)
+);
+
+select * from customer;
+select * from phone_number;
